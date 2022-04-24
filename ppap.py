0e934b910051ad8a5fbe728b86aabcd8661fa39b
5zaqws <96614445+5zaqws@users.noreply.github.com>
2022-04-24 16:34:58 -0700

    ppap.py
    Main operating python code.

---
+++ ppap.py
@@ -0,0 +1,41 @@
+from string import ascii_lowercase as ascii_lowercase
+from string import ascii_uppercase as ascii_uppercase
+import math
+
+
+def integer_to_negative_value(integer):
+    return math.sqrt(math.pow(integer, 2)) - math.sqrt(math.pow(integer, 2)) - math.sqrt(math.pow(integer, 2))
+
+
+def integer_to_absolute(integer):
+    integer = integer_to_negative_value(integer)
+    return int(integer) + int(integer) * -1 + int(integer) * -1
+
+
+def lowercase_to_uppercase(string):
+    new_string = ''
+    for char in list(string):
+        char_index = ascii_lowercase.find(char)
+        char_index = integer_to_absolute(char_index)  # prevent -1 value
+        new_char = ascii_uppercase.__getitem__(char_index)
+        new_string = new_string + new_char
+    return new_string
+
+
+def print_ppap():
+    char_list = []
+    for index in list({1, 2, 3, 4}):
+        current_char = 'a' if 2 < index < 4 else 'p'
+        char_index = ascii_lowercase.find(current_char)
+        char_index = integer_to_absolute(char_index)  # prevent -1 value
+        new_char = lowercase_to_uppercase(ascii_lowercase.__getitem__(char_index))
+        char_list.append(new_char)
+
+    new_string = ''
+    for char in tuple(char_list):
+        new_string = new_string + char
+
+    print(new_string)
+
+
+print_ppap()
